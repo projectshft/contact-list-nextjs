081d4a2a89803a8bfe34347ccb8a0fd79702f2a4
AdamC7313 <AdamC7313@gmail.com>
2023-12-16 13:53:29 -0500

    initial commit, add bootstrap

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
       <body className={inter.className}>{children}</body>
     </html>
   )
