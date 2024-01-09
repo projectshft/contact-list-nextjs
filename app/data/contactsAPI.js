ee220aab70b3c9d39ed271a54c4fbd0cb91d231e
Ryan Hardin <rhardin378@gmail.com>
2024-01-09 04:12:23 -0600

    added dummy data to api

---
+++ app/data/contactsAPI.js
@@ -0,0 +1,36 @@
+export const contactAPI = {
+  contacts: [
+    {
+      id: 70219577,
+      name: "Albert Einstein",
+      image_url:
+        "https://en.wikipedia.org/wiki/Albert_Einstein#/media/File:Einstein_1921_by_F_Schmutzer_-_restoration.jpg",
+      email: "aeinstein@example.com",
+      phone_number: "15555555555",
+    },
+    {
+      id: 90210325,
+      name: "Isaac Newton",
+      image_url:
+        "https://en.wikipedia.org/wiki/Isaac_Newton#/media/File:Portrait_of_Sir_Isaac_Newton,_1689_(brightened).jpg",
+      email: "inewton@example.com",
+      phone_number: "16666666666",
+    },
+    {
+      id: 80230445,
+      name: "Marie Curie",
+      image_url:
+        "https://upload.wikimedia.org/wikipedia/commons/c/c8/Marie_Curie_c._1920s.jpg",
+      email: "mcurie@example.com",
+      phone_number: "17777777777",
+    },
+    {
+      id: 60890123,
+      name: "Nikola Tesla",
+      image_url:
+        "https://en.wikipedia.org/wiki/Nikola_Tesla#/media/File:N.Tesla.JPG",
+      email: "ntesla@example.com",
+      phone_number: "18888888888",
+    },
+  ],
+};
--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-
+import ContactList from "./contacts/page";
 export default function Home() {
-  return (
-    <main>
-      <h1>home</h1>
-    </main>
-  );
+  return <ContactList />;
 }
