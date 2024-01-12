ac59a222fc3aa88c94e7423150d0e0b611ff84ef
Ryan Hardin <rhardin378@gmail.com>
2024-01-12 16:05:59 -0600

    in the Contact component in /contacts/[id]/page used useParams hook to get access to the routes id parameter an API call is made using this id by passing this id into my contactsAPI getById method.  The page can then use the specific contact's information when the page is rendered.  if the id passed into the route param is not a valid id the page will inform you its not valid and provide a back button

--- app/contacts/[id]/page.js
+++ app/contacts/[id]/page.js
@@ -1,12 +1,38 @@
+"use client";
+import { contactsAPI } from "../../data/contactsAPI";
 import Link from "next/link";
+import { useParams } from "next/navigation";
+
 export default function Contact() {
+  const { id } = useParams();
+  const contact = contactsAPI.getById(parseInt(id, 10));
+
+  if (!contact) {
+    return (
+      <div className="text-danger text-center">
+        Sorry, but the Contact could not be found
+        <Link className="btn btn-secondary mb-2" href="/contacts">
+          Back
+        </Link>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h1>Contact info</h1>
-      <Link className="btn btn-secondary mb-2" href="/contacts">
-        Back
-      </Link>
-      <h2></h2>
+    <div className="container">
+      <div className="row justify-content-center text-center">
+        <h1>Contact info</h1>
+        <Link className="btn btn-secondary w-auto mb-2" href="/contacts">
+          Back
+        </Link>
+        <h2>{contact.name}</h2>
+        <img
+          className="contact-image"
+          src={contact.image_url}
+          alt={`image of ${contact.name}`}
+        />
+        <span>{contact.email}</span>
+        <span>{contact.phone_number}</span>
+      </div>
     </div>
   );
 }
