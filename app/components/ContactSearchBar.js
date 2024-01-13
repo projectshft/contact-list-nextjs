78f1caf43a9acc08b5bb1d61203f3242ccd0fcd8
Ryan Hardin <rhardin378@gmail.com>
2024-01-12 19:13:23 -0600

    ContactSearchBar gets a searchTearm from the searchBar input and passes the search term to our onSearchTerm function.  Our contactSearch function is   is passed from our Contacts Component as the prop onSearchTermChangee

--- app/components/ContactSearchBar.js
+++ app/components/ContactSearchBar.js
@@ -1,7 +1,18 @@
-export default function ContactSearchBar() {
+"use client";
+import { useState } from "react";
+export default function ContactSearchBar({ onSearchTermChange }) {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div>
-      <input className="form-control m-2 text-center" type="text" placeholder="Search By Name" />
+      <input
+        className="form-control my-2  text-center"
+        type="text"
+        placeholder="Search By Name"
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+          onSearchTermChange(searchTerm);
+        }}
+      />
     </div>
   );
 }
